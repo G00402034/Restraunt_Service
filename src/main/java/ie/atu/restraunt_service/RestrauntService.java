66315756e0736b3f0af5c657c065eb96ca7b5ee0
G00402034 <G00402034@atu.ie>
2024-11-07 19:18:27 +0000

    added methods for updating, deleting and finding avalibility restraunt

--- src/main/java/ie/atu/restraunt_service/RestrauntService.java
+++ src/main/java/ie/atu/restraunt_service/RestrauntService.java
@@ -19,5 +19,39 @@ public class RestrauntService {
         public Optional<Restraunt> getRestaurantById(String id) {
             return restrauntRepository.findById(id);
         }
+
+        public Restraunt updateRestaurant(String id, Restraunt restaurantData) {
+            Optional<Restraunt> restaurantOptional = restrauntRepository.findById(id);
+            if (restaurantOptional.isPresent()) {
+                Restraunt restaurant = restaurantOptional.get();
+                restaurant.setName(restaurantData.getName());
+                restaurant.setAddress(restaurantData.getAddress());
+                restaurant.setContactNumber(restaurantData.getContactNumber());
+                restaurant.setCuisine(restaurantData.getCuisine());
+                restaurant.setOperatingHours(restaurantData.getOperatingHours());
+                restaurant.setMenu(restaurantData.getMenu());
+                restaurant.setRating(restaurantData.getRating());
+                return restrauntRepository.save(restaurant);
+            }
+            throw new RuntimeException("Restaurant not found");
+        }
+
+        public void deleteRestaurant(String id) {
+            restrauntRepository.deleteById(id);
+        }
+
+        public boolean checkAvailability(String id) {
+            Optional<Restraunt> restaurant = restrauntRepository.findById(id);
+            return restaurant.map(Restraunt::isAvailable).orElse(false);
+        }
+
+        public void addReview(String id, Review review) {
+            Optional<Restraunt> restaurantOptional = restrauntRepository.findById(id);
+            restaurantOptional.ifPresent(restaurant -> {
+                restaurant.getReviews().add(review);
+                restrauntRepository.save(restaurant);
+            });
+        }
     }
-}
+    }
+
