9cc3cd9e1664c6293f3e29d3ae507f17c10d17c7
G00402034 <G00402034@atu.ie>
2024-11-07 16:29:52 +0000

    Initial commit

---
+++ src/test/java/ie/atu/restraunt_service/RestrauntServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.restraunt_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestrauntServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
