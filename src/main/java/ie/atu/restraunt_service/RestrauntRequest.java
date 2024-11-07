8c0b5ac535913ae243d720e0845aee2d307766fb
G00402034 <G00402034@atu.ie>
2024-11-07 19:53:17 +0000

    added data for restraunt in RestrauntRequest

--- src/main/java/ie/atu/restraunt_service/RestrauntRequest.java
+++ src/main/java/ie/atu/restraunt_service/RestrauntRequest.java
@@ -1,4 +1,17 @@
 package ie.atu.restraunt_service;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class RestrauntRequest {
+
+    private String name;
+    private String address;
+    private String contactNumber;
+    private List<String> cuisine;
+    private OperatingHours operatingHours;
+    private List<MenuItem> menu;
+
 }
